6b0da84ee773fd81bc3c1ad7b65e6daf3e72ff8a
Carlos Henrique Brunetti Junior <carlos.brunetti@hotmail.com>
2020-05-14 17:55:32 -0300

    colocando imagens

---
+++ QuickBuy.Web/ClientApp/src/app/usuario/login/login.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+
+@Component({
+  selector: "app-login",
+  templateUrl: "./login.component.html",
+  styleUrls:["./login.component.css"]
+})
+
+export class LoginComponent {
+  public enderecoImagem = "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcS8E1xJX9lszSM2uVPIDREBMA_D4_drf6IgMYKs381l6rezSnO0&usqp=CAU";
+}
